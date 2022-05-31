2892bc3651d829cb4c6915cf7eb816a440720c59
da8865892q <da8865892q@gmail.com>
2022-06-01 03:55:27 +0800

    Update submodule content sync to repository

--- function.js
+++ function.js
@@ -2,7 +2,7 @@
  * @param  {string} str input the text you want to return.
  */
  function log(str) {
-  return `Yor input is: ${str}`
+  return `Yor input content is: ${str}`
 }
 
 module.exports = {
